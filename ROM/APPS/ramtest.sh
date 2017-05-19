dfc94d01799f211dd786b33e1c0ad0da3c4f7aae
sashz <23256427+pdaxrom@users.noreply.github.com>
2017-05-19 19:14:26 +0000

    external ram tests

--- ROM/APPS/ramtest.sh
+++ ROM/APPS/ramtest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-writefile=/tmp/$$$.bin
-readfile=/tmp/$$$.bin
+writefile=$(tempfile)
+readfile=$(tempfile)
 
-dd if=/dev/urandom of=$writefile bs=1 count=53248
+ram_test() {
+    dd if=/dev/urandom of=$writefile bs=1 count=53248 &>/dev/null
+    ../bootloader /dev/ttyUSB0 load $writefile 1000
 
-../bootloader /dev/ttyUSB0 load $writefile 1000
+    ../bootloader /dev/ttyUSB0 save $readfile 1000 e000
+    cmp -s $writefile $readfile && echo "RAM TEST OKAY" || echo "RAM TEST FAILED"
+}
 
-../bootloader /dev/ttyUSB0 save $readfile 1000 e000
-cmp $writefile $readfile && echo "RAM write/read OKAY!"
-
-../bootloader /dev/ttyUSB0 save $readfile 1000 e000
-cmp $writefile $readfile && echo "RAM write/read OKAY!"
-
-../bootloader /dev/ttyUSB0 save $readfile 1000 e000
-cmp $writefile $readfile && echo "RAM write/read OKAY!"
+ram_test
+ram_test
+ram_test
+ram_test
 
 rm -f $writefile $readfile
