f58352a7694a2b07110a6f50b60dd65116d98b63
sashz <23256427+pdaxrom@users.noreply.github.com>
2017-06-17 14:50:41 +0000

    replace asm hex sign $ to 0x

--- ROM/DEVEL/exporthdr.sh
+++ ROM/DEVEL/exporthdr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-awk '/\yequ|EQU\y/ { print "#define\t"$1"\t"$3; }' $1
+awk '/\yequ|EQU\y/ { print "#define\t"$1"\t"$3; }' $1 | \
+    awk '{ tmp=match($0, /\$[0-9A-Za-z]+/); if (tmp != 0) print substr($0,0,tmp-1)"0x"substr($0,tmp+1,length($0)-tmp); else print $0; }'
